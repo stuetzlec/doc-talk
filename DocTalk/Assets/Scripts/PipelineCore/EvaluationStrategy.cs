672055e941bf66de8a99ac236c0fd2689aa016a5
tones <thetoneman@outlook.com>
2024-03-13 14:53:09 -0400

    Added Documentation.

--- DocTalk/Assets/Scripts/PipelineCore/EvaluationStrategy.cs
+++ DocTalk/Assets/Scripts/PipelineCore/EvaluationStrategy.cs
@@ -1,10 +1,21 @@
 namespace PipelineCore
 {
-    interface EvaluationStrategy<T>
+    /// <summary>
+    /// Represents a strategy for evaluating responses in a pipeline.
+    /// </summary>
+    /// <typeparam name="T">The type of evaluation result.</typeparam>
+    public interface EvaluationStrategy<T>
     {
+        /// <summary>
+        /// Evaluates a response message.
+        /// </summary>
+        /// <param name="responseMessage">The response message to be evaluated.</param>
         void EvaluateResponse(ResponseMessage responseMessage);
 
-        T getEvaluation();
-
+        /// <summary>
+        /// Gets the evaluation result.
+        /// </summary>
+        /// <returns>The evaluation result of type <typeparamref name="T"/>.</returns>
+        T GetEvaluation();
     }
 }
--- DocTalk/Assets/Scripts/PipelineCore/Evaluator.cs
+++ DocTalk/Assets/Scripts/PipelineCore/Evaluator.cs
@@ -1,23 +1,38 @@
-using System;
-
 namespace PipelineCore
 {
+    /// <summary>
+    /// Represents a feedback collector that uses an evaluation strategy to evaluate responses.
+    /// </summary>
     public class FeedbackCollector
     {
         private EvaluationStrategy evaluator;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeedbackCollector"/> class.
+        /// </summary>
+        /// <param name="evaluator">The evaluation strategy to be used by the feedback collector.</param>
         public FeedbackCollector(EvaluationStrategy evaluator)
         {
             this.evaluator = evaluator;
         }
 
+        /// <summary>
+        /// Evaluates a response using the evaluation strategy.
+        /// </summary>
+        /// <param name="response">The response message to be evaluated.</param>
         public void EvaluateResponse(ResponseMessage response)
         {
-            evaluator.EvaluateResponse(ResponseMessage response);
+            evaluator.EvaluateResponse(response);
         }
 
-        public T getEvaluation() {
-           return evaluator.getEvaluation();
+        /// <summary>
+        /// Retrieves the evaluation result from the evaluation strategy.
+        /// </summary>
+        /// <returns>The evaluation result.</returns>
+        public T GetEvaluation()
+        {
+            return evaluator.GetEvaluation();
         }
     }
 }
+
