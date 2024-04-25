cdb18247ab8bab26792afc99e4beac77b985bb19
William Hancock <williamhancock328@gmail.com>
2024-04-25 15:31:07 -0400

    Commenting out PC code for testing on VR

--- DocTalk/Assets/Button/Click.cs
+++ DocTalk/Assets/Button/Click.cs
@@ -6,16 +6,16 @@ public class Click : MonoBehaviour
 
     public int sceneIndex;
 
-   Ray ray;
-	RaycastHit hit;
+    //Ray ray;
+	//RaycastHit hit;
 	
 	void Update()
 	{
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(ray, out hit))
-		{
-			if(Input.GetMouseButtonDown(0))
+		//ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//if(Physics.Raycast(ray, out hit))
+		//{
+			//if(Input.GetMouseButtonDown(0))
 				SceneManager.LoadScene(sceneIndex);
-		}
+		//}
 	}
 }
