5b7518550c415756b528c81248ba82fe47d74f92
William Hancock <williamhancock328@gmail.com>
2024-04-27 14:38:20 -0400

    Added another female character

--- DocTalk/Assets/Scripts/NPCShuffler.cs
+++ DocTalk/Assets/Scripts/NPCShuffler.cs
@@ -6,22 +6,31 @@ public class NPCShuffler : MonoBehaviour
 {
     public GameObject object1;
     public GameObject object2;
+    public GameObject object3;
 
     void Start()
     {
-        // Generate a random number (0 or 1)
-        int randomIndex = Random.Range(0, 2);
+        // Generate a random number (0 or 3)
+        int randomIndex = Random.Range(0, 3);
 
         // Enable one object and disable the other based on the random number
         if (randomIndex == 0)
         {
             object1.SetActive(true);
             object2.SetActive(false);
+            object3.SetActive(false);
+        }
+        else if (randomIndex == 1)
+        {
+            object1.SetActive(false);
+            object2.SetActive(true);
+            object3.SetActive(false);
         }
         else
         {
             object1.SetActive(false);
-            object2.SetActive(true);
+            object2.SetActive(false);
+            object3.SetActive(true);
         }
     }
 
