a6b80b16b29909b0638919f9a12e4fc91622ab64
William Hancock <williamhancock328@gmail.com>
2024-04-28 20:07:27 -0400

    ready for tone man

--- DocTalk/Assets/Scripts/feedbackswitcher.cs
+++ DocTalk/Assets/Scripts/feedbackswitcher.cs
@@ -26,6 +26,7 @@ public class feedbackswitcher : MonoBehaviour
         {
             transcriptCollector.GetTranscript();
             transcriptCollector.SaveToFile();
+            transcriptCollector.FeedbackAlgo();
             LoadNextScene();
         }
     }
