32011094d6ffcf980c41f0de5b81a81165fa2e4e
tone-man <thetoneman@outlook.com>
2024-04-30 11:18:40 -0400

    Basic Keywords

---
+++ DocTalk/Assets/Scripts/Keywords.cs
@@ -0,0 +1,74 @@
+// ApologyPhrases.cs contains keywords and phrases to be tallied by the feedback collector.
+
+public static class ApologyPhrases
+{
+    // Words and phrases to avoid in apologies
+    public static readonly string[] WordsToAvoid = new string[]
+    {
+        "but",
+        "however",
+        "if",
+        "might",
+        "maybe",
+        "perhaps",
+        "sorry if",
+        "I'm sorry but",
+        "I'm sorry you feel",
+        "I'm sorry you think",
+        "I'm sorry that you",
+        "we apologize if",
+        "it was not my intention",
+        "it's not my fault",
+        "you should have",
+        "you misunderstood",
+        "it's not our responsibility",
+        "that's not what I meant",
+        "this is just a misunderstanding",
+        "don't take it personally",
+        "calm down",
+        "let's move on",
+        "I was just joking",
+        "no offense, but",
+        "I didn't mean it that way",
+        "this isn't a big deal",
+        "you're overreacting",
+        "it's your fault",
+        "this is how things are",
+        "everyone makes mistakes"
+    };
+
+    // Contrapositive phrases for constructive apologies
+    public static readonly string[] ContrapositivePhrases = new string[]
+    {
+        "and",
+        "I understand your perspective",
+        "regardless of",
+        "definitely",
+        "I'm certain",
+        "this is clear",
+        "I'm sorry that",
+        "I'm sorry for",
+        "I'm sorry about",
+        "we apologize for",
+        "we're taking responsibility",
+        "it's our responsibility",
+        "I need to correct my words",
+        "this requires immediate attention",
+        "your feelings are valid",
+        "I understand your frustration",
+        "let's address this",
+        "I take responsibility",
+        "I need to make amends",
+        "I'm listening to you",
+        "your feedback is important",
+        "thank you for bringing this up",
+        "I recognize my mistake",
+        "how can we improve?",
+        "I appreciate your patience",
+        "I take this seriously",
+        "how can I make this right?",
+        "I value our relationship",
+        "we're in this together",
+        "everyone deserves respect"
+    };
+}
