b98da91837625fcc9526faa4ae914797a27c72a8
William Hancock <williamhancock328@gmail.com>
2024-05-01 23:30:27 -0400

    demo

---
+++ DocTalk/Assets/Click.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Click : MonoBehaviour
+{
+
+    public int sceneIndex;
+
+   Ray ray;
+	RaycastHit hit;
+	
+	void Update()
+	{
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit))
+		{
+			if(Input.GetMouseButtonDown(0))
+				SceneManager.LoadScene(sceneIndex);
+		}
+	}
+}
